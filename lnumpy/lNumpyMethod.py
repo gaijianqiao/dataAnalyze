01f5de80cf3b9cb35dd488a788b4831752768a23
Gai Super <gaichao@julong.cc>
2018-06-21 16:54:41 +0800

    add numpy content

--- lnumpy/lNumpyMethod.py
+++ lnumpy/lNumpyMethod.py
@@ -4,6 +4,24 @@ print(tArray)
 print(type(tArray))
 print(tArray.ndim)
 print(np.random.rand(10).reshape(2,5))
+# 将1000个数 做成一个10*10*10的一个三维数组中
 threeArray = np.random.rand(1000).reshape(10,10,10)
-print(threeArray)
+
+# print(threeArray.ndim)
+linSpaceArray = np.linspace(start=10,stop=20,num=10,endpoint=True,retstep=False)
+# print(linSpaceArray)
+
+
+zerosThreeArray = np.zeros_like(threeArray)
+# print(zerosThreeArray)
+
+
+zeroByShape = np.zeros(threeArray.shape)
+oneByShape = np.ones(threeArray.shape)
+# print(oneByShape)
+# print(zeroByShape)
+# 创建一个数组，对角线为1其他的为0
+eyesArray = np.eye(5,5,dtype=np.int32)
+print(eyesArray)
+
 
---
+++ lnumpy/lNumpyShape.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+lArray = np.arange(1,11)
+# print(lArray)
+bArray = lArray.reshape(2,5)
+# print(bArray)
+# print(lArray is bArray)
+# 转置
+btArray = bArray.T
+print(bArray)
+print(btArray)
+
+
+mAArray = np.arange(10).reshape(2,5)
+print(mAArray)
+mBArray = np.arange(10).reshape(5,2)
+print(mBArray)
+mCArray = mBArray * mAArray
+print(mCArray)
+
+
