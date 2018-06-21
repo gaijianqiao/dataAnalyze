e684e933bb3405209313e0449b01d7307ed3f71d
Gai Super <gaichao@julong.cc>
2018-06-21 11:07:11 +0800

    learning data analyze

---
+++ lnumpy/lNumpyMethod.py
@@ -0,0 +1,9 @@
+import numpy as np
+tArray = np.array([['a',1,'3',4],['a','2','3',4]])
+print(tArray)
+print(type(tArray))
+print(tArray.ndim)
+print(np.random.rand(10).reshape(2,5))
+threeArray = np.random.rand(1000).reshape(10,10,10)
+print(threeArray)
+
